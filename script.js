6fb4c1ac3bb868d77836e38718e7b9bd6b507f4a
Shraddha kapse <99063249+Shraddhakapse22@users.noreply.github.com>
2024-03-23 16:41:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const inputbox=document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+
+function AddTask(){
+    if(inputbox.value === ''){
+        alert("you must write something");
+
+    }else{
+        let li=document.createElement("li");
+        li.innerHTML=inputbox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value=" ";
+    saveDate();
+
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveDate();
+    }
+    else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+     saveDate();
+}
+},false);
+
+function saveDate(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
